11312b35feab1098d2ae817974a517a50973eeac
Estephany <71114445+Estephanys000@users.noreply.github.com>
2025-10-14 23:00:34 +0000

    Criação da classe Empresa

---
+++ estagios/src/main/java/mack/ps2/estagios/estagios/model/Empresa.java
@@ -0,0 +1,12 @@
+package mack.ps2.estagios.estagios.model;
+
+public class Empresa {
+    private String nome;
+    private int anoFundacao;
+
+    public Empresa(String nome, int anoFundacao) {
+        this.nome = nome;
+        this.anoFundacao = anoFundacao;
+    }
+
+}
