4ba75695e4f746568e6820f595f8ce5c584f4bad
Estephany <71114445+Estephanys000@users.noreply.github.com>
2025-10-25 03:39:22 +0000

    Alterações em Empresa.java e criação da EmpresaRepo.java

---
+++ estagios/src/main/java/mack/ps2/estagios/estagios/repositories/EmpresaRepo.java
@@ -0,0 +1,13 @@
+// src/main/java/mack/ps2/estagios/estagios/repositories/EstudanteRepo.java
+// src/main/java/mack/ps2/estagios/estagios/repositories/EmpresaRepo.java
+package mack.ps2.estagios.estagios.repositories;
+
+import mack.ps2.estagios.estagios.model.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpresaRepo extends JpaRepository<Empresa, Long> {
+    // JpaRepository já fornece
+    // findAll(), findById(), save(), deleteById(), etc.
+}
